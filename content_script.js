23e13f17bb5ff1769d4a06f2442edf320d04ab3c
millatime1010 <millatime1010@gmail.com>
2021-12-03 11:30:57 -0800

    Add ignore scene option

--- content_script.js
+++ content_script.js
@@ -16,9 +16,33 @@ async function sleepUntil(f, timeoutMs) {
   });
 }
 
+function ignoreScene(linkElement, replaceIcon) {
+  linkElement.addClass("stash_id_ignored");
+  const cardElement = linkElement.parents(".card");
+  const sceneImage = cardElement.find(".SceneCard-image");
+  sceneImage.css("opacity", ".25");
+  cardElement.css("background-color", "rgba(48, 64, 77, 0.25)");
+
+  if(replaceIcon) {
+    const imageElement = linkElement.find(".stash_id_match img");
+    imageElement.attr("src", chrome.runtime.getURL('images/ignore.svg'));
+  }
+}
+
+function clearIgnore(linkElement) {
+  linkElement.removeClass("stash_id_ignored");
+  const cardElement = linkElement.parents(".card");
+  const sceneImage = cardElement.find(".SceneCard-image");
+  sceneImage.css("opacity", "1");
+  cardElement.css("background-color", "rgba(48, 64, 77, 1)");
+  const imageElement = linkElement.find(".stash_id_match img");
+  imageElement.attr("src", chrome.runtime.getURL('images/red_x.svg'));
+}
+
 async function run() {
   const store = await chrome.storage.sync.get();
   const address = store.address ? store.address : "http://localhost:9999";
+  const ignoredScenes = store.ignoredScenes ? store.ignoredScenes : {};
  
   try {
     await sleepUntil(() => {
@@ -43,17 +67,87 @@ async function run() {
       let new_image = $("<img>", { style: "height: 20px; width: 20px;"});
       display.append(new_link);
       new_link.append(new_image);
-      if(results.data.findScenes.scenes.length) {
-        new_image.attr('src', chrome.runtime.getURL('images/green_check.svg'));
-        new_link.attr('href', address + "/scenes/" + results.data.findScenes.scenes[0].id);
+      if(id in ignoredScenes) {
+        new_image.attr('src', chrome.runtime.getURL('images/ignore.svg'));
+        ignoreScene(linkElement, false);
       } else {
-        new_image.attr('src', chrome.runtime.getURL('images/red_x.svg'));
+        if(results.data.findScenes.scenes.length) {
+          new_image.attr('src', chrome.runtime.getURL('images/green_check.svg'));
+          new_link.attr('href', address + "/scenes/" + results.data.findScenes.scenes[0].id);
+        } else {
+          new_image.attr('src', chrome.runtime.getURL('images/red_x.svg'));
+        }
       }
     
       new_link.on("click", (event) => {
         event.stopImmediatePropagation();
       });
-  
+
+      display.hover(
+        function() {
+          let dropdown = $("<div>", { 
+            class:"menu", 
+            style: "height: 55px; width: 40px; background: transparent; position: fixed; z-index: 1"});
+
+          const top = $(this).offset().top - $(document).scrollTop()
+          dropdown.css("top", (top - 10) + 'px')
+          dropdown.css("left", $(this).offset().left + 'px')
+
+          let menu = $("<div>", { class: "dropdown-menu", style: "display: block; background: white; position: absolute; top: 0; left: 25px"});
+
+          dropdown.on("click", (event) => {
+            event.preventDefault();
+            event.stopImmediatePropagation();
+          });
+
+          if(new_link.attr('href') !== undefined) {
+            let gotoScene =  $("<a>", { class: "dropdown-item"});
+            gotoScene.text("Go to Scene");
+
+            gotoScene.on("click", (event) => {
+              event.preventDefault();
+              event.stopImmediatePropagation();
+              location.href = new_link.attr('href');
+            });
+            menu.append(gotoScene);
+          }
+          if(new_link.attr('href') === undefined) {
+            let ignoreLink = $("<a>", { class: "dropdown-item"});
+
+            if(linkElement.hasClass("stash_id_ignored")) {
+              ignoreLink.text("Clear Ignore");
+              
+              ignoreLink.on("click", (event) => {
+                event.preventDefault();
+                event.stopImmediatePropagation();
+                delete ignoredScenes[id];
+                chrome.storage.sync.set({"ignoredScenes": ignoredScenes});
+                clearIgnore(linkElement);
+                menu.remove();
+              });
+
+              menu.append(ignoreLink);
+            } else {
+              ignoreLink.text("Ignore Scene");
+
+              ignoreLink.on("click", (event) => {
+                event.preventDefault();
+                event.stopImmediatePropagation();
+                ignoredScenes[id] = true;
+                chrome.storage.sync.set({"ignoredScenes": ignoredScenes});
+                ignoreScene(linkElement, true);
+                menu.remove();
+              });
+            }
+            menu.append(ignoreLink);
+          }
+          dropdown.append(menu);
+          $(this).append(dropdown);
+        },
+        function() {
+          $(this).find(".menu").remove();
+        }
+      );
       linkElement.find('.stash_id_match').remove();
       linkElement.append(display);
     });
